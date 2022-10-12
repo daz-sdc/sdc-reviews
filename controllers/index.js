edd8593075248867156373cbb198f9f9d3d9d74b
Dongning Song <dongningsong@dongnings-mini.lan>
2022-10-12 13:43:59 -0600

    keep working on query

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,24 @@
+/* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
-// Hndles request flow
+// Handles request flow
 // Never handles data logic
+const models = require('../models/index');
+
 exports.getReviews = (req, res) => {
-  console.log(req.query.product_id);
-  res.send('OK!!');
+  const page = Number(req.query.page) || 1;
+  const count = Number(req.query.count) || 5;
+  const sort = (!['newest', 'helpful', 'relevant'].includes(req.query.sort)) ? 'relevant' : req.query.sort;
+  const productId = req.query.product_id;
+  console.log(page, count, productId, sort);
+  res.send('getReviews!!');
 };
 
-
 exports.getReviewsMeta = (req, res) => {
-
+  const productId = req.query.product_id;
+  res.send('getReviewsMeta!!');
 };
 
 exports.postReviews = (req, res) => {
-
 };
 
 exports.putReviewsHelpfulness = (req, res) => {
--- models/index.js
+++ models/index.js
@@ -1,2 +1,10 @@
 // Handles data logic
 // Interacts with database
+const db = require('../db/index');
+
+exports.getReviews = () => {
+  const text = `SELECT r.product, r.rating, r.summary, r.recommend, r.response, r.body, r.date,
+   r.reviewer_name, r.helpfulness, rp.photo_id, rp.url, rp.review_id
+  FROM reviews r JOIN reviews_photos as rp ON r.id = rp.review_id WHERE r.product = $71702`;
+  return db.query();
+};
