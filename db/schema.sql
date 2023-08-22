789a9ebcb0dad45c3ae1b389644435e7c790049c
Dongning Song <mathdsong@gmail.com>
2023-08-21 18:44:49 -0600

    create a materialized view for metadata

--- db/schema.sql
+++ db/schema.sql
@@ -91,24 +91,223 @@ LEFT JOIN
   ) rp
 ON rp.review_id = r.review_id;
 
+CREATE MATERIALIZED VIEW mv_meta_tb
+AS
+SELECT
+	s1.product_id, s1.ratings, s2.recommended, s3.characteristics
+FROM (
+	SELECT
+		product_id, jsonb_object_agg (value, CAST(count_char_value AS VARCHAR)) AS ratings
+	FROM (
+		SELECT
+			c.product_id, cr.value, COUNT(cr.value) AS count_char_value
+		FROM  characteristic_reviews cr
+		LEFT JOIN characteristics c
+		ON cr.characteristic_id = c.id
+		GROUP BY c.product_id, cr.value
+	) s11
+	GROUP BY s11.product_id
+) s1
+FULL OUTER JOIN (
+	SELECT
+		product_id, jsonb_object_agg (recommend, CAST(count_recommend AS VARCHAR)) AS recommended
+	FROM (
+		SELECT
+		product_id, recommend, COUNT(recommend) AS count_recommend
+		FROM reviews
+		GROUP BY product_id, recommend
+	) s22
+	GROUP BY s22.product_id
+) s2
+ON s1.product_id = s2.product_id
+FULL OUTER JOIN (
+	SELECT product_id, jsonb_object_agg(name, overall_characteristics) AS characteristics
+	FROM (
+		SELECT product_id, name, jsonb_build_object('id', char_id, 'value', CAST(avg_value AS VARCHAR)) AS overall_characteristics
+		FROM (
+			SELECT c.product_id, c.id as char_id, c.name, CAST(SUM(cr.value) AS DECIMAL) / COUNT(c.id) as avg_value
+			FROM characteristic_reviews cr
+			LEFT JOIN characteristics c
+			ON cr.characteristic_id = c.id
+			GROUP BY char_id, c.product_id
+		) t
+	) tt
+	GROUP BY product_id
+) s3
+ON s3.product_id = s1.product_id;
+
 
 
 -- -- experimental queries:
 
+SELECT jsonb_build_object('1', '3', '2', '1', '3', '1', '4', '2', '5', '8');
 
--- this query works but very slow, update 100 records will cost 5min 51s:
-do
-$$
-BEGIN
-  FOR review_id in 1..(SELECT COUNT(id) FROM reviews) LOOP
-    UPDATE reviews
-    SET rank_helpfulness = (SELECT rank FROM (SELECT id, rank() OVER (PARTITION BY product_id ORDER BY helpfulness DESC) FROM reviews) t WHERE id = review_id)
-    WHERE id = review_id;
-  END LOOP;
-END;
-$$;
+SELECT c.product_id, cr.characteristic_id, c.name, cr.value FROM characteristic_reviews cr
+RIGHT JOIN characteristics c
+ON cr.characteristic_id = c.id
+WHERE c.product_id < 5;
+
+SELECT c.product_id, cr.value, COUNT(cr.value)
+FROM characteristic_reviews cr
+LEFT JOIN characteristics c
+ON cr.characteristic_id = c.id
+WHERE c.product_id < 5
+GROUP BY cr.value, c.product_id
+ORDER BY c.product_id;
+
+SELECT c.product_id,
+jsonb_build_object(
+cr.value, COUNT(cr.value)
+)
+FROM characteristic_reviews cr
+LEFT JOIN characteristics c
+ON cr.characteristic_id = c.id
+WHERE c.product_id < 5
+GROUP BY cr.value, c.product_id
+ORDER BY c.product_id;
+
+SELECT
+product_id,
+jsonb_object_agg (value, count) ratings
+FROM (
+SELECT c.product_id, cr.value, COUNT(cr.value)
+FROM characteristic_reviews cr
+LEFT JOIN characteristics c
+ON cr.characteristic_id = c.id
+WHERE c.product_id < 10
+GROUP BY cr.value, c.product_id
+ORDER BY c.product_id
+) t
+GROUP BY product_id;
+
+SELECT product_id, recommend, COUNT(recommend)
+FROM reviews
+WHERE product_id < 10
+GROUP BY product_id, recommend
+ORDER BY product_id;
+
+SELECT
+product_id,
+jsonb_object_agg (recommend, count) recommended
+FROM (
+SELECT product_id, recommend, COUNT(recommend)
+FROM reviews
+WHERE product_id < 10
+GROUP BY product_id, recommend
+ORDER BY product_id
+) t
+GROUP BY product_id;
+
+SELECT s1.product_id, s1.ratings, s2.recommended
+FROM (
+SELECT
+product_id, jsonb_object_agg (value, count) AS ratings
+FROM (
+SELECT
+c.product_id, cr.value, COUNT(cr.value)
+FROM  characteristic_reviews cr
+LEFT JOIN characteristics c
+ON cr.characteristic_id = c.id
+GROUP BY c.product_id, cr.value
+) s11
+GROUP BY s11.product_id
+) s1
+FULL OUTER JOIN (
+SELECT product_id, jsonb_object_agg (recommend, count) AS recommended
+FROM (
+SELECT
+product_id, recommend, COUNT(recommend)
+FROM reviews
+GROUP BY product_id, recommend
+) s22
+GROUP BY s22.product_id
+) s2
+ON s1.product_id = s2.product_id
+GROUP BY s1.product_id;
 
 
+SELECT c.product_id, c.id, cr.value
+FROM characteristic_reviews cr
+LEFT JOIN characteristics c
+ON cr.characteristic_id = c.id
+WHERE c.product_id < 10;
 
+
+SELECT c.product_id, c.id as char_id, c.name, cast(sum(cr.value) as decimal) / count(c.id) as value
+FROM characteristic_reviews cr
+LEFT JOIN characteristics c
+ON cr.characteristic_id = c.id
+WHERE c.product_id < 10
+GROUP BY char_id, c.product_id;
+
+
+SELECT product_id, jsonb_build_object('id', char_id, 'value', value)
+FROM (
+	SELECT c.product_id, c.id as char_id, c.name, cast(sum(cr.value) as decimal) / count(c.id) as value
+	FROM characteristic_reviews cr
+	LEFT JOIN characteristics c
+	ON cr.characteristic_id = c.id
+	WHERE c.product_id < 10
+	GROUP BY char_id, c.product_id
+) t
+
+
+SELECT product_id, jsonb_object_agg(name, overall_characteristics) AS characteristics
+FROM (
+	SELECT product_id, name, jsonb_build_object('id', char_id, 'value', avg_value) AS overall_characteristics
+	FROM (
+		SELECT c.product_id, c.id as char_id, c.name, cast(sum(cr.value) as decimal) / count(c.id) as avg_value
+		FROM characteristic_reviews cr
+		LEFT JOIN characteristics c
+		ON cr.characteristic_id = c.id
+		WHERE c.product_id < 10
+		GROUP BY char_id, c.product_id
+	) t
+) tt
+GROUP BY product_id
+
+
+SELECT
+	s1.product_id, s1.ratings, s2.recommended, s3.characteristics
+FROM (
+	SELECT
+		product_id, jsonb_object_agg (value, count) AS ratings
+	FROM (
+		SELECT
+			c.product_id, cr.value, COUNT(cr.value)
+		FROM  characteristic_reviews cr
+		LEFT JOIN characteristics c
+		ON cr.characteristic_id = c.id
+		GROUP BY c.product_id, cr.value
+	) s11
+	GROUP BY s11.product_id
+) s1
+FULL OUTER JOIN (
+	SELECT
+		product_id, jsonb_object_agg (recommend, count) AS recommended
+	FROM (
+		SELECT
+		product_id, recommend, COUNT(recommend)
+		FROM reviews
+		GROUP BY product_id, recommend
+	) s22
+	GROUP BY s22.product_id
+) s2
+ON s1.product_id = s2.product_id
+FULL OUTER JOIN (
+	SELECT product_id, jsonb_object_agg(name, overall_characteristics) AS characteristics
+	FROM (
+		SELECT product_id, name, jsonb_build_object('id', char_id, 'value', avg_value) AS overall_characteristics
+		FROM (
+			SELECT c.product_id, c.id as char_id, c.name, cast(sum(cr.value) as decimal) / count(c.id) as avg_value
+			FROM characteristic_reviews cr
+			LEFT JOIN characteristics c
+			ON cr.characteristic_id = c.id
+			GROUP BY char_id, c.product_id
+		) t
+	) tt
+	GROUP BY product_id
+) s3
+ON s3.product_id = s1.product_id;
 
 
